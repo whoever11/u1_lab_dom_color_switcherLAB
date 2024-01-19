7d5f33cbcff7a7f512c69cc015fe9bddf4f854d8
whoever11 <cjthesedays000@gmail.com>
2024-01-19 12:03:43 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
 // Write your DOM code here!
+console.log('working')
+
+// red button
+const red = document.querySelector('.red')
+const redElement = document.querySelector('.red')
+
+red.addEventListener('click', () => {
+    document.body.classList.toggle('red')
+    document.body.classList.remove('blue', 'white', 'yellow')
+    document.querySelector('h1').style.color = 'white'
+    document.querySelector('p').style.color = 'white'
+})
+
+// white button
+const white = document.querySelector('.white')
+const whiteElement = document.querySelector('.white')
+
+white.addEventListener('click', () => {
+    document.body.classList.toggle('white')
+    document.body.classList.remove('red', 'blue', 'yellow')
+    document.querySelector('h1').style.color = 'black'
+    document.querySelector('p').style.color = 'black'
+})
+
+// blue button
+const blue = document.querySelector('.blue')
+const blueElement = document.querySelector('.blue')
+
+blue.addEventListener('click', () => {
+    document.body.classList.toggle('blue')
+    document.body.classList.remove('red', 'white', 'yellow')
+    document.querySelector('h1').style.color = 'white'
+    document.querySelector('p').style.color = 'white'
+})
+
+// yellow button
+const yellow = document.querySelector('.yellow')
+const yellowElement = document.querySelector('.yellow')
+
+yellow.addEventListener('click', () => {
+    document.body.classList.toggle('yellow')
+    document.body.classList.remove('red', 'blue', 'white')
+    document.querySelector('h1').style.color = 'black'
+    document.querySelector('p').style.color = 'black'
+})
