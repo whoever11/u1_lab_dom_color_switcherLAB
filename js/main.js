a812508e8dd12d04ba8ed2fa224893a558096266
whoever11 <cjthesedays000@gmail.com>
2024-01-19 12:28:40 -0500

    final change

--- js/main.js
+++ js/main.js
@@ -8,9 +8,15 @@ const redElement = document.querySelector('.red')
 red.addEventListener('click', () => {
     document.body.classList.toggle('red')
     document.body.classList.remove('blue', 'white', 'yellow')
-    document.querySelector('h1').style.color = 'white'
-    document.querySelector('p').style.color = 'white'
-})
+   
+    if (document.body.classList.contains('red')) {
+        document.querySelector('h1').style.color = 'white'
+        document.querySelector('p').style.color = 'white'
+    } else {
+        document.querySelector('h1').style.color = 'black'
+        document.querySelector('p').style.color = 'black'
+    }
+}) // Added closing parenthesis here
 
 // white button
 const white = document.querySelector('.white')
@@ -18,9 +24,15 @@ const white = document.querySelector('.white')
 white.addEventListener('click', () => {
     document.body.classList.toggle('white')
     document.body.classList.remove('red', 'blue', 'yellow')
-    document.querySelector('h1').style.color = 'black'
-    document.querySelector('p').style.color = 'black'
-})
+   
+    if (document.body.classList.contains('white')) {
+        document.querySelector('h1').style.color = 'black'
+        document.querySelector('p').style.color = 'black'
+    } else {
+        document.querySelector('h1').style.color = 'black'
+        document.querySelector('p').style.color = 'black'
+    }
+}) // Added closing parenthesis here
 
 // blue button
 const blue = document.querySelector('.blue')
@@ -28,9 +40,15 @@ const blue = document.querySelector('.blue')
 blue.addEventListener('click', () => {
     document.body.classList.toggle('blue')
     document.body.classList.remove('red', 'white', 'yellow')
-    document.querySelector('h1').style.color = 'white'
-    document.querySelector('p').style.color = 'white'
-})
+   
+    if (document.body.classList.contains('blue')) {
+        document.querySelector('h1').style.color = 'white'
+        document.querySelector('p').style.color = 'white'
+    } else {
+        document.querySelector('h1').style.color = 'black'
+        document.querySelector('p').style.color = 'black'
+    }
+}) // Added closing parenthesis here
 
 // yellow button
 const yellow = document.querySelector('.yellow')
@@ -38,6 +56,12 @@ const yellow = document.querySelector('.yellow')
 yellow.addEventListener('click', () => {
     document.body.classList.toggle('yellow')
     document.body.classList.remove('red', 'blue', 'white')
-    document.querySelector('h1').style.color = 'black'
-    document.querySelector('p').style.color = 'black'
+   
+    if (document.body.classList.contains('yellow')) {
+        document.querySelector('h1').style.color = 'black'
+        document.querySelector('p').style.color = 'black'
+    } else {
+        document.querySelector('h1').style.color = 'black'
+        document.querySelector('p').style.color = 'black'
+    }
 })
